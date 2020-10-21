1be823061a3202106b69279c60025e7442b71053
Il Seok Oh <iloh@microsoft.com>
2020-10-21 18:36:07 +0900

    temperature ramp down

--- device-simulation/windturbine-state.js
+++ device-simulation/windturbine-state.js
@@ -708,15 +708,24 @@ function main(context, previousState, previousProperties) {
         if (state.convTemp < 85) state.convTemp += 2.0;
         else state.convTemp = 85.0;
     }
-    else state.convTemp = next[2];
+    else 
+    {
+        if (state.convTemp > next[2]) state.convTemp -= 2.0;
+        else state.convTemp = next[2];
+    }
+    
 
     if (properties.gearbox == "temp-error") 
     {
         if (state.gearboxTemp < 85) state.gearboxTemp += 2.0;
         else state.gearboxTemp = 85.0;
     }
-    else state.gearboxTemp = next[3];
-
+    else 
+    {
+        if (state.gearboxTemp > next[3]) state.gearboxTemp -= 2.0;
+        else state.gearboxTemp = next[3];
+    }
+    
     if (properties.generator == "alert") state.power = 0;
     else if (properties.gearbox == "alert") state.power = (next[12] * 0.3).toFixed(2);
     else state.power = next[12];
