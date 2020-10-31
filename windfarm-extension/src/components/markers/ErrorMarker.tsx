cdb6a5993b650217c9980408f97a21f62b282688
Bao Ton <Bao.Ton@Bentley.com>
2020-10-31 09:31:58 -0700

    Add an appending list.

--- windfarm-extension/src/components/markers/ErrorMarker.tsx
+++ windfarm-extension/src/components/markers/ErrorMarker.tsx
@@ -52,12 +52,33 @@ function ErrorList({turbinePower}: any) {
     }
   })
 
+  const addToList = () => {
+    const list = document.getElementById('list');
+    const newLI = document.createElement('li');
+    newLI.innerHTML = 'A new item';
+    newLI.className = newLI.className;
+    list!.insertBefore(newLI, list!.firstChild);
+    setTimeout(function () {
+      newLI.className = newLI.className + " show";
+    }, 10);
+  }
+
+
   return (
     <div style={{zIndex: 100}}>
-      <p>Turbine: {power.id} </p>
-      <p>Observed Power: {power.observedPower}</p>
-      <p>Physical Model power: {power.physicalPower}</p>
-      <p>Data Model power: {power.datamodelPower}</p>
+      <div>
+        <p>Turbine: {power.id} </p>
+        <p>Observed Power: {power.observedPower.toFixed(2)}</p>
+        <p>Physical Model power: {power.physicalPower.toFixed(2)}</p>
+        <p>Data Model power: {power.datamodelPower.toFixed(2)}</p>
+      </div>
+      <div className="slide-fade">
+        <ul id="list">
+          <li className="show">List item</li>
+          <li className="show">List item</li>
+        </ul>
+        <button id="add-to-list" onClick={addToList}>Add a list item</button>
+      </div>
     </div>
   )
 }
