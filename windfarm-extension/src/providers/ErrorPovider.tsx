3defa13df42d05ed6158a338a38eb5313bdb8eb4
BaoTon <39310001+BaoTon@users.noreply.github.com>
2020-11-05 14:43:00 -0800

    Stylistic updates (#9)
    * Add new styles
    
    * Use a single card on click within powermarker.
    
    * Toggle the component's detailed layout when clicking on alerts.
    
    * Cards for temperature and event.
    
    * Reposition and add sensor and temperature data.
    
    * WIP temperature marker.
    
    * Reposition wind marker logic.
    
    * Cleanup and remove decorators when an alert is clicked.
    
    * Change power card alert error.
    
    * Change temperature cards.
    
    * Fix titles of power and temperature alerts.
    
    * Cleanup unused code.

--- windfarm-extension/src/providers/ErrorPovider.tsx
+++ windfarm-extension/src/providers/ErrorPovider.tsx
@@ -9,6 +9,7 @@ import { PowerDecorator } from "../components/decorators/PowerDecorator";
 import { WindfarmExtension } from "../WindfarmExtension";
 import { IModelApp, StandardViewId } from "@bentley/imodeljs-frontend";
 import { TemperatureMarker } from "../components/markers/TemperatureMarker";
+import { Point3d } from "@bentley/geometry-core";
 
 // The lists continue to grow but we shouldn't pollute the DOM.
 const MAX_ELEMENTS = 12;
@@ -45,22 +46,21 @@ export function AggregateErrorList() {
     function onErrorClick(markerId: string, errorType: string) {
 
       PowerDecorator.markers.forEach((marker) => {
+
+        if (markerId !== marker.id) {
+          IModelApp.viewManager.dropDecorator(marker.sensorData);
+          IModelApp.viewManager.dropDecorator(marker.windData);
+          IModelApp.viewManager.dropDecorator(marker.temperatureData);
+        }
+
         if (markerId === marker.id && errorType === "Power Alert") {
+
           WindfarmExtension.viewport?.zoomToElements([marker.cId, marker.sId, marker.bId], { animateFrustumChange: true, standardViewId: StandardViewId.Right });
           ReactDOM.unmountComponentAtNode(document.getElementById("error-component")!);
           ReactDOM.render(<DetailedPowerErrorList turbinePower={marker}></DetailedPowerErrorList>, document.getElementById("error-component"));
           return;
         } else if (markerId === marker.id && errorType === "Temperature Alert") {
-          WindfarmExtension.viewport?.zoomToElements([marker.cId, marker.sId, marker.bId], { animateFrustumChange: true, standardViewId: StandardViewId.Back });
-
-          // Remove all other markers.
-          PowerDecorator.markers.forEach(marker => {
-            IModelApp.viewManager.dropDecorator(marker.sensorData);
-            IModelApp.viewManager.dropDecorator(marker.windData);
-            IModelApp.viewManager.dropDecorator(marker.temperatureData);
-          });
-
-          IModelApp.viewManager.addDecorator(marker.temperatureData);
+          WindfarmExtension.viewport?.zoomToElements([marker.cId, marker.sId, marker.bId], { animateFrustumChange: true, standardViewId: StandardViewId.Right });
 
           ReactDOM.unmountComponentAtNode(document.getElementById("error-component")!);
           ReactDOM.render(<DetailedTemperatureErrorList turbineTemperature={marker.temperatureData.marker}></DetailedTemperatureErrorList>, document.getElementById("error-component"));
@@ -85,24 +85,41 @@ export function AggregateErrorList() {
         {error.isCurrent ?
          error.errorType === "Power Alert" ? 
           <li className="show-power-error" onClick={() => onErrorClick(error.id, error.errorType)}>
-            <div>Turbine: {error.id} <br></br>
-              Type: {error.errorType} <br></br>
-              <span className="time-caption">Since {timeStart}</span>
+            <div className="card-alert priority-2">
+              <svg viewBox="0 0 20 20">
+                <path d="m2.5 2.5h15v15h-15z" fill="#f60"/>
+                <path d="m17 3v14h-14v-14zm1-1h-16v16h16z"/>
+                <path d="m9 14h-2v-2h2zm0-8h-2v4.66667h2zm4 8h-2v-2h2zm0-8h-2v4.66667h2z"/>
+              </svg>
+              <p className="title">{error.errorType}</p>
+              <p className="id">ID: {error.id}</p>
+              <p className="metadata">Since {timeStart}</p>
             </div>
           </li>
          :
           <li className="show-temp-error" onClick={() => onErrorClick(error.id, error.errorType)}>
-            <div>Turbine: {error.id} <br></br>
-              Type: {error.errorType}
+            <div className="card-alert priority-3">
+                <svg viewBox="0 0 20 20">
+                <path d="m.902 17.5 9.098-14.557 9.098 14.557z" fill="#fff200"/>
+                <path d="m10 3.8868 8.19575 13.1132h-16.3915zm0-1.8868-10 16h20z"/>
+                <path d="m11 15h-2v-2h2zm0-8h-2v4.66667h2z"/>
+              </svg>
+              <p className="title">{error.errorType}</p>
+              <p className="id">ID: {error.id}</p>
+              <p className="metadata">Since {timeStart}</p>
             </div>
-              <span className="time-caption">Since {timeStart}</span>
           </li>
           :
           <li className="show-non-error" onClick={() => onErrorClick(error.id, error.errorType)}>
-            <div>Turbine: {error.id} <br></br>
-              Type: {error.errorType.replace("Alert", "Event")}
-              <span className="time-caption">Start: {timeStart}</span>
-              <span className="time-caption">End: {timeEnd}</span>
+            <div className="card-alert priority-4">
+                <svg viewBox="0 0 16 16">
+                <circle cx="8" cy="8" fill="#0ff" r="7.5"/>
+                <path d="m8 1a7 7 0 1 1 -7 7 7 7 0 0 1 7-7m0-1a8 8 0 1 0 8 8 8 8 0 0 0 -8-8zm1 12h-2v-2h2zm0-8h-2v4.66667h2z"/>
+              </svg>
+              <p className="title">{error.errorType.replace("Alert", "Event")}</p>
+              <p className="id">ID: {error.id}</p>
+              <p className="metadata">Start: {timeStart}</p>
+              <p className="metadata">End: {timeEnd}</p>
             </div>
           </li>
         }
@@ -160,20 +177,17 @@ export function DetailedPowerErrorList({ turbinePower }: any) {
       >
 
       <li className="show-power">
-        <table style={{borderCollapse: "collapse"}} cellSpacing="0" cellPadding="0">
-          <tr>
-            <td>
-              <u>{date}</u> <br></br> {time}
-            </td>
-            <td>
-              <ul>
-                  <li> OB: {error.powerObserved?.toFixed(2)} </li>
-                  <li> PM: {error.powerPM?.toFixed(2)} </li>
-                  <li> DM: {error.powerDM?.toFixed(2)} </li>
-              </ul>
-            </td>
-          </tr>
-        </table>
+          <div className="card-power-alert priority-2">
+            <svg viewBox="0 0 20 20">
+              <path d="m2.5 2.5h15v15h-15z" fill="#f60" />
+              <path d="m17 3v14h-14v-14zm1-1h-16v16h16z" />
+              <path d="m9 14h-2v-2h2zm0-8h-2v4.66667h2zm4 8h-2v-2h2zm0-8h-2v4.66667h2z" />
+            </svg>
+            <p className="title"><u>{date}</u> at {time}</p>
+            <p className="metadata"><b>Observed Power</b>: {error.powerObserved?.toFixed(2)} kW</p>
+            <p className="metadata"><b>Physical Power</b>: {error.powerPM?.toFixed(2)} kW</p>
+            <p className="metadata"><b>Data Model Power</b>: {error.powerDM?.toFixed(2)} kW</p>
+          </div>
       </li>
     </CSSTransition>
   )
@@ -181,19 +195,19 @@ export function DetailedPowerErrorList({ turbinePower }: any) {
 
   return (
     <div>
-      <svg className="minimize-error-panel" onClick={deactivateWidget}>
-        <use href="/imjs_extensions/windfarm/icons.svg#minimize"></use>
-        <title>Minimize</title>
-      </svg>
-      <button style={{float: "right", marginTop: "10px"}} onClick={displayAggregate}>Back</button>
-      <div className="rcorners">
-        <h3 style={{margin: "0", marginBottom: "-13px"}}><u>Turbine: {power.id}</u></h3> <br />
-        Observed: {power.observedPower.toFixed(2)} <br />
-        Physical: {power.physicalPower.toFixed(2)} <br />
-        Data Model: {power.datamodelPower.toFixed(2)} <br />
+      <div className="card-power-status priority-5">
+        <svg className="minimize-error-panel" style={{padding: 0, marginRight: "15px"}} onClick={deactivateWidget}>
+          <use href="/imjs_extensions/windfarm/icons.svg#minimize"></use>
+          <title>Minimize</title>
+        </svg>
+        <svg className="back-button" onClick={() => displayAggregate()} viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="m11 3a1.00006 1.00006 0 0 1 1 1v8a.99991.99991 0 0 1 -1.4961.8682l-7.0001-4a1 1 0 0 1 0-1.7364l7.0001-4a.99862.99862 0 0 1 .4961-.1318z"/></svg>
+        <p className="title"><b>Turbine ID: <u>{power.id}</u></b></p>
+        <p className="metadata"><b>Observed Power</b>: {power.observedPower?.toFixed(2)} kW</p>
+        <p className="metadata"><b>Physical Power</b>: {power.physicalPower?.toFixed(2)} kW</p>
+        <p className="metadata"><b>Data Model Power</b>: {power.datamodelPower?.toFixed(2)} kW</p>
       </div>
       <div>
-        <h3 style={{margin: "0", marginBottom: "-13px"}}><u>Warnings:</u></h3> <br />
+        <h3 style={{margin: "0", marginBottom: "-13px"}}><u>Power Warnings:</u></h3> <br />
         <ul id="list">
           <TransitionGroup>
             {items}
@@ -235,20 +249,17 @@ export function DetailedTemperatureErrorList({ turbineTemperature }: any) {
       >
 
       <li className="show-temp">
-        <table style={{borderCollapse: "collapse"}} cellSpacing="0" cellPadding="0">
-          <tr>
-            <td>
-              <u>{date}</u> <br></br> {time}
-            </td>
-            <td>
-              <ul>
-                  <li> NA: {error.tempNacelle?.toFixed(2)} </li>
-                  <li> GN: {error.tempGenerator?.toFixed(2)} </li>
-                  <li> GB: {error.tempGearBox?.toFixed(2)} </li>
-              </ul>
-            </td>
-          </tr>
-        </table>
+          <div className="card-power-alert priority-3">
+            <svg viewBox="0 0 20 20">
+              <path d="m.902 17.5 9.098-14.557 9.098 14.557z" fill="#fff200" />
+              <path d="m10 3.8868 8.19575 13.1132h-16.3915zm0-1.8868-10 16h20z" />
+              <path d="m11 15h-2v-2h2zm0-8h-2v4.66667h2z" />
+            </svg>
+            <p className="title"><u>{date}</u> at {time}</p>
+            <p className="metadata"><b>Nacelle Temp.</b>: {error.tempNacelle?.toFixed(2)} °C</p>
+            <p className="metadata"><b>Generator Temp.</b>: {error.tempGenerator?.toFixed(2)} °C</p>
+            <p className="metadata"><b>Gear Box Temp.</b>: {error.tempGearBox?.toFixed(2)} °C</p>
+          </div>
       </li>
     </CSSTransition>
   )
@@ -256,19 +267,19 @@ export function DetailedTemperatureErrorList({ turbineTemperature }: any) {
 
   return (
     <div>
-      <svg className="minimize-error-panel" onClick={deactivateWidget}>
-        <use href="/imjs_extensions/windfarm/icons.svg#minimize"></use>
-        <title>Minimize</title>
-      </svg>
-      <button style={{float: "right", marginTop: "10px"}} onClick={displayAggregate}>Back</button>
-      <div className="rcorners">
-        <h3 style={{margin: "0", marginBottom: "-13px"}}><u>Turbine: {temperature.id}</u></h3> <br />
-        Nacelle: {temperature.tempNacelle.toFixed(2)} <br />
-        Generator: {temperature.tempGenerator.toFixed(2)} <br />
-        GearBox: {temperature.tempGearBox.toFixed(2)} <br />
+      <div className="card-power-status priority-5">
+        <svg className="minimize-error-panel" style={{padding: 0, marginRight: "15px"}} onClick={deactivateWidget}>
+          <use href="/imjs_extensions/windfarm/icons.svg#minimize"></use>
+          <title>Minimize</title>
+        </svg>
+        <svg className="back-button" onClick={() => displayAggregate()} viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="m11 3a1.00006 1.00006 0 0 1 1 1v8a.99991.99991 0 0 1 -1.4961.8682l-7.0001-4a1 1 0 0 1 0-1.7364l7.0001-4a.99862.99862 0 0 1 .4961-.1318z"/></svg>
+        <p className="title"><b>Turbine ID: <u>{temperature.id}</u></b></p>
+        <p className="metadata"><b>Temp. Nacelle</b>: {temperature.tempNacelle?.toFixed(2)} °C</p>
+        <p className="metadata"><b>Temp. Generator</b>: {temperature.tempGenerator?.toFixed(2)} °C</p>
+        <p className="metadata"><b>Temp. Gear Box</b>: {temperature.tempGearBox?.toFixed(2)} °C</p>
       </div>
       <div>
-        <h3 style={{margin: "0", marginBottom: "-13px"}}><u>Warnings:</u></h3> <br />
+        <h3 style={{margin: "0", marginBottom: "-13px"}}><u>Temperature Warnings:</u></h3> <br />
         <ul id="list">
           <TransitionGroup>
             {items}
