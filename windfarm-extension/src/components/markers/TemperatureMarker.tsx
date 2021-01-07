91faaa6e38dbdb6740a49181846d856d9ee1523a
Bao Ton <Bao.Ton@Bentley.com>
2021-01-06 20:14:46 -0800

    Cleanup.

--- windfarm-extension/src/components/markers/TemperatureMarker.tsx
+++ windfarm-extension/src/components/markers/TemperatureMarker.tsx
@@ -155,12 +155,6 @@ export class TemperatureMarker extends Marker {
 
   public drawFunc(_ctx: CanvasRenderingContext2D) {
 
-    /*
-    if (FrontstageManager.activeFrontstageDef!.bottomPanel!.panelState === StagePanelState.Open) {
-      this.worldLocation = new Point3d(this.powerMarker.worldLocation.x, this.powerMarker.worldLocation.y + 120, this.powerMarker.worldLocation.z)
-    }
-    */
-
     const props = {
       onHover: this.hover,
       tempGearBox: this.temperatureGearBox,
